95e09bf2cc226fad051043d078703f572e376c9f
VermiNew <werminew@gmail.com>
2026-08-10 11:52:29 +0200

    --amend

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 $PWT_TOOLKIT_SCRIPTS_FOLDER_PATH = Join-Path (Split-Path $PROFILE) "Scripts"
 . "$PWT_TOOLKIT_SCRIPTS_FOLDER_PATH\_pwt-core.ps1"
-Initialize-PwtLazyRegistry -ScriptsDir $Scripts
+Initialize-PwtLazyRegistry -ScriptsDir $PWT_TOOLKIT_SCRIPTS_FOLDER_PATH
