b892619ccd29dcb70c5e69c9760fc3f020066877
VermiNew <werminew@gmail.com>
2026-08-10 11:51:39 +0200

    fix: change scripts value for a less known one to avoid conflicts with other scripts/programs

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-$Scripts = Join-Path (Split-Path $PROFILE) "Scripts"
-. "$Scripts\_pwt-core.ps1"
+$PWT_TOOLKIT_SCRIPTS_FOLDER_PATH = Join-Path (Split-Path $PROFILE) "Scripts"
+. "$PWT_TOOLKIT_SCRIPTS_FOLDER_PATH\_pwt-core.ps1"
 Initialize-PwtLazyRegistry -ScriptsDir $Scripts
